875b4a65c7c17f8abfe7075dd001ac2ee280cbc8
Christian Blom <christian.blom@chasacademy.se>
2019-09-07 21:27:49 +0200

    Login page created, actually logging in against the backend is not implemented yet.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Login() {
+	const [formData, setFormData] = useState({
+		email: '',
+		password: ''
+	});
+
+	const { email, password } = formData;
+
+	const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		console.log("Logged in!");
+	}
+
 	return (
-		<div>
-			Login
-		</div>
+		<main className="general-main login">
+			<h2 className="center-text">Please sign in below</h2>
+			<form className="login-form" onSubmit={(e) => onSubmit(e)}>
+				<input
+					className="mb-4"
+					type="email"
+					placeholder="Email address"
+					name="email"
+					value={email}
+					onChange={(e) => onChange(e)}
+					required
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					name="password"
+					value={password}
+					onChange={(e) => onChange(e)}
+					required
+				/>
+				<button type="submit" className="btn btn-primary mt-4 btn-medium">LOGIN</button>
+			</form>
+			<footer className="register-button">
+				<p>Don&apos;t have an account?</p>
+				<Link to="/register">
+					<button type="button" className="btn btn-secondary btn-medium">REGISTER</button>
+				</Link>
+			</footer>
+		</main>
 	)
 }
