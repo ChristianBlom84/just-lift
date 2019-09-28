c9390a40b2ac1fa78e39150ebf51f69174ddfd71
Christian Blom <christian.blom@chasacademy.se>
2019-09-28 13:36:20 +0200

    Added error logging with Winston and winston-express

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('dotenv').config({ path: './deploy/.env' });
+const logger = require('./loggers/logger');
+const errorLogger = require('./loggers/errorLogger');
 
 const connectDB = require('./config/db');
 
 const app = express();
+const router = express.Router();
 
 const corsOptions = {
 	origin: process.env.ALLOW_ORIGIN
@@ -29,10 +32,15 @@ app.use(cors(corsOptions));
 app.get('/', (req, res) => res.send('API running'));
 
 // Define Routes
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/exercises', require('./routes/api/exercises'));
-app.use('/api/workouts', require('./routes/api/workouts'));
-app.use('/api/history', require('./routes/api/history'));
+app.use(router);
+router.use('/api/auth', require('./routes/api/auth'));
+router.use('/api/exercises', require('./routes/api/exercises'));
+router.use('/api/workouts', require('./routes/api/workouts'));
+router.use('/api/history', require('./routes/api/history'));
+
+// Loggers
+app.use(logger);
+app.use(errorLogger);
 
 const PORT = process.env.PORT || 5000;
 
